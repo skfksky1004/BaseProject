e0ff83ca4797124055e7aa8f51fefb031e564c38
jyoon@cheritz.com <jyoon@cheritz.com>
2021-03-29 09:07:01 +0900

    - ButtonEx.cs 작성중

---
+++ BaseProject/Assets/Script/UI/TextUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextUI : MonoBehaviour
+{
+    [SerializeField] private ButtonEx _btnTest;
+
+    private void Awake()
+    {
+        if (_btnTest.ButtonIcon != null)
+        {
+            
+        }
+    }
+}
