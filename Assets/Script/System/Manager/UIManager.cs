df30249b827c537d0af547f31fa7b77ca2958a09
jyoon@cheritz.com <jyoon@cheritz.com>
2021-04-05 09:19:59 +0900

    - 싱글톤 추가

---
+++ Assets/Script/System/Manager/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoSingleton<UIManager>
+{
+    public override bool Initialize()
+    {
+        return true;
+    }
+
+    protected override void Destroy()
+    {
+        
+    }
+}
---
+++ Assets/Script/System/MonoSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : Component
+{
+    public static T I => Instance;
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null)
+                    instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            }
+
+            return instance;
+        }
+    }
+
+    public void SetParent(Transform tf)
+    {
+        if(tf == null)
+            return;
+        
+        this.transform.SetParent(tf);
+    }
+
+    public abstract bool Initialize();
+    protected abstract void Destroy();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void OnApplicationQuit()
+    {
+        Destroy();
+    }
+}
